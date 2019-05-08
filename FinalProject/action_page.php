cb7d6e6ed6cb5704d84c993af253474a90cbaed7
Darkpooh <49340026+Darkpooh@users.noreply.github.com>
2019-05-07 23:52:22 -0500

    Add files via upload

---
+++ FinalProject/action_page.php
@@ -0,0 +1,9 @@
+<!Doctype html>
+<body>
+
+<form action="action_page.php" method="post">
+Thank you <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?> we will be in contact.
+
+</body>
+</html>
